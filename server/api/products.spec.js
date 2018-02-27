fb0b16565096ff6c5fcf1820100dfd0f671d3543
Dongwoo Kang <schach0415@yahoo.com>
2018-02-27 14:02:11 -0500

    POST test

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -6,12 +6,12 @@ const db = require('../db')
 const app = require('../index')
 const Product = db.model('product')
 
-describe('Product routes', () => {
+describe('Product routes without a seed data', () => {
     beforeEach(() => {
         return db.sync({force: true})
     })
 
-    describe('GET /api/products', () => {
+    describe('`/api/products` URI', () => {
         it('GET responds with an empty array at first', () => {
             // when we make requests to `/api/products` we will get back an empty array
             return request(app)
@@ -24,7 +24,61 @@ describe('Product routes', () => {
         })
     })
 
-    describe('GET `/api/products`', () => {
+    describe('`/api/products` URI', () => {
+        const fakeProduct = {
+            name: 'yamiyami',
+            ingredients: ['carrot'],
+            price: 10,
+            timeToPrep: 0,
+            numberInStock: 2,
+            calories: 0,
+            description: 'what do you think this is'
+        }
+        const otherProduct = {
+            name: 'kakibox',
+            ingredients: ['mushroom'],
+            price: 20,
+            timeToPrep: 3,
+            numberInStock: 100,
+            calories: 20,
+            description: 'mountain mushroom'
+        }
+
+        beforeEach(() => {
+            return Product.create( fakeProduct )
+        })
+
+        it('GET responds with a product has been added', () => {
+            return request(app)
+                .get('/api/products')
+                .expect(200)
+                .expect('Content-Type', /json/) // tests response header
+                .then(res => {
+                    expect(res.body).to.be.an('array')
+                    expect(res.body[0].name).to.be.equal(fakeProduct.name)
+            })
+        })
+
+        it('POST creates a product', () => {
+            return request(app)
+                .post('/api/products')
+                .send(otherProduct)
+                .expect(201)
+                .expect('Content-Type', /json/)
+                .expect(res => {
+                    expect(res.body).to.be.an('object')
+                    expect(res.body.name).to.equal(otherProduct.name)
+                    expect(res.body.price).to.equal(otherProduct.price)
+                    expect(res.body.calories).to.equal(otherProduct.calories)
+                    expect(res.body.description).to.equal(otherProduct.description)
+                    expect(res.body.numberInStock).to.equal(otherProduct.numberInStock)
+                    expect(res.body.timeToPrep).to.equal(otherProduct.timeToPrep)
+                    expect(res.body.ingredients[0]).to.equal(otherProduct.ingredients[0])
+            })
+        })
+    })
+    
+    describe('`/api/products/:productId` URI', () => {
         const fakeProduct = {
             name: 'yamiyami',
             ingredients: ['carrot'],
@@ -39,17 +93,18 @@ describe('Product routes', () => {
             return Product.create( fakeProduct )
         })
 
-        it('GET `/api/products` responds with a product after a task has been added', () => {
+        it('GET responds with a specitic product has been added', () => {
             return request(app)
-                .get('/api/products')
+                .get('/api/products/1')
                 .expect(200)
-                .expect('Content-Type', /json/) // tests response header
+                .expect('Content-Type', /json/)
                 .then(res => {
-                    expect(res.body).to.be.an('array')
-                    expect(res.body[0].name).to.be.equal('yamiyami')
+                    expect(res.body).to.be.an('object')
+                    expect(res.body.name).to.equal(fakeProduct.name)
+                    expect(res.body.price).to.equal(fakeProduct.price)
+                    expect(res.body.calories).to.equal(fakeProduct.calories)
+                    expect(res.body.description).to.equal(fakeProduct.description)
             })
         })
-
-        // it('GET `/api/products/:productId`', ())
     })
 })
