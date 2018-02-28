416bb1b59199ee106c657cc92485b618e0d8c0f6
Domnik Mack <domnikmack@gmail.com>
2018-02-28 15:19:12 -0500

    made order status required

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,8 +1,14 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Order = db.define('orders', {
-  stats: Sequelize.ENUM('cart', 'paid', 'shipped') // eslint-disable-line new-cap
+const Order = db.define('order', {
+  status: {
+    type: Sequelize.ENUM('cart', 'paid', 'shipped'), // eslint-disable-line new-cap
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
 });
 
 module.exports = Order
