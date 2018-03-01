6a911827d0d403643a2910415355dbed30c9e5bb
Domnik Mack <domnikmack@gmail.com>
2018-03-01 14:36:14 -0500

    line-item routes

--- server/api/index.js
+++ server/api/index.js
@@ -7,6 +7,8 @@ router.use('/users', require('./users'))
 
 router.use('/order', require('./order'))
 
+router.use('/line-items', require('./lineItems'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
