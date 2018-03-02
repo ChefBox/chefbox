e64ec0bf65c5fcc66dc9b528b3bcff4d4de68803
queerviolet <ashi@queerlyamorous.org>
2018-03-02 10:53:15 -0500

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 module.exports = router
 
+//// in server/api/cart.js
 // Cart:
 //   1. Unauthed user, find or create an Order and somehow assoc with
 //      the current session.
@@ -13,9 +14,13 @@ async function withCart(req, res, next) {
 
   // 2. There is a cartId on the session
   if (req.session.cartId) {
-    // Look up that order, and put it on req.cart
+    // TODO: Look up that order, and put it on req.cart
     return next()
   }
+  
+  req.cart = await Order.cartForUser(req.user)
+  req.session.cartId = req.cart.id
+  next()
 
   // 3. There isn't a cartId on the session, so we have to
   //    create a cart
@@ -23,6 +28,18 @@ async function withCart(req, res, next) {
   //    b. Somebody is logged in (req.user is a User)
   //      i. They already have an Order that is in state: CART
   //      ii. They don't.
+}
+
+//// in server/db/models/order.js
+Order.cartForUser = function(user) {
+  if (!user) return Order.create()
+  // Something like this...
+  return Order.findOrCreate({
+    where: {
+      userId: user.id,
+      status: 'cart',
+    }
+  })
 }
 
 router.use('/products', require('./products'))
