aafc6c694b8433b4cb6b132e15549074386d0b9d
Dongwoo Kang <schach0415@yahoo.com>
2018-03-03 11:49:35 -0500

    done review store

---
+++ client/store/reviews.js
@@ -0,0 +1,89 @@
+import axios from 'axios';
+
+/**
+ * ACTION TYPES
+ */
+const GET_REVIEWS = 'GET_REVIEWS';
+const CREATE_REVIEW = 'CREATE_REVIEW';
+const UPDATE_REVIEW = 'UPDATE_REVIEW';
+const DELETE_REVIEW = 'DELETE_REVIEW';
+
+/**
+ * ACTION CREATORS
+ */
+
+const getReviews = (reviews) => {
+  return {
+    type: GET_REVIEWS,
+    reviews
+  }
+}
+
+const createReview = (review) => {
+  return {
+    type: CREATE_REVIEW,
+    review
+  }
+}
+
+const updateReview = (review) => {
+  return {
+    type: UPDATE_REVIEW,
+    review
+  }
+}
+
+const deleteReview = (id) => {
+  return {
+    type: DELETE_REVIEW,
+    id
+  }
+}
+
+/**
+ * THUNK CREATORS
+ */
+
+export const fetchReviews = () => dispatch => {
+    axios.get('/api/reviews')
+        .then(res => res.data)
+        .then(reviews => dispatch(getReviews(reviews)))
+        .catch(err => console.error('Fetching Reviews unsuccesful.', err))
+}
+
+export const addReview = review => dispatch => {
+    axios.post('/api/reviews', review)
+        .then(res => res.data)
+        .then(newReview => dispatch(updateReview(newReview))
+        .catch(err => console.error(`Creating Review ${review} unsuccesful.`, err))
+}
+
+export const editReview = (review, id) => dispatch => {
+    axios.put(`/api/reviews/${id}`, review)
+        .then(res => res.data)
+        .then(editedReview => dispatch(updateReview(editedReview)))
+        .catch(err => console.error(`Updating Review ${review} unsuccesful.`, err))
+}
+
+export const removeReview = id => dispatch => {
+    axios.delete(`/api/reviews/${id}`)
+        .then(() => dispatch(deleteReview(id)))
+        .catch(err => console.error(`Deleting Review (id: ${id}) unsuccesful.`, err))
+}
+
+export default function reducer(Reviews = [], action) {
+  switch (action.type) {
+    case GET_REVIEWS:
+      return action.reviews;
+    case CREATE_REVIEW:
+      return [...reviews, action.review];
+    case UPDATE_REVIEW:
+      return reviews.map(review => {
+        return review.id === action.review.id ? action.review : review
+      });
+    case DELETE_REVIEW:
+      return reviews.filter(review => review.id !== action.review.id);
+    default:
+      return reviews;
+  }
+}
