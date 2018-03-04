901d39003e3a6cb4cfb1ab6978761a2884049d91
Domnik Mack <domnikmack@gmail.com>
2018-03-04 18:43:05 -0500

    innitial footer

---
+++ client/components/footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => (
+  <div className = "footer">
+    <div>
+    <ul>
+    <li><a href="#" >Link 1</a></li>
+    <li><a href="#" >Link 2</a></li>
+    <li><a href="#" >Link 3</a></li>
+    </ul>
+    </div>
+    <div>
+    <ul>
+    <li><a href="#" >Link 4</a></li>
+    <li><a href="#" >Link 5</a></li>
+    <li><a href="#" >Link 6</a></li>
+    </ul>
+    </div>
+  </div>
+)
+
+
+export default Footer
