a2167b0ce138852512abb13d3f12ffff9dd39d1e
Aaron Young <brainomite@gmail.com>
2018-03-04 07:47:34 -0500

    fixed error in merge conflict resolution

--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome, ProductDetail, ProductEdit, ProductCreate, ProductList, Cart} from './components'
-import {me, fetchCategories, fetchProducts, fetchOrders} from './store'
+import {me, fetchCategories, fetchProducts, fetchOrders, fetchReviews} from './store'
 import CategoryCreate from './components/Category/CategoryCreate';
 import RemoveCategory from './components/Category/RemoveCategory';
 
