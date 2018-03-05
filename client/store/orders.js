0e077bcc482051754a2b7813361ce16db8943588
Domnik Mack <domnikmack@gmail.com>
2018-03-05 15:33:11 -0500

    added remove buttons

--- client/store/orders.js
+++ client/store/orders.js
@@ -7,10 +7,6 @@ import axios from 'axios';
 const GET_CART = 'GET_CART'
 const ADD_TO_CART = 'ADD_TO_CART';
 const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
-const GET_ORDERS = 'GET_ORDERS';
-const CREATE_ORDER = 'CREATE_ORDER';
-const UPDATE_ORDER = 'UPDATE_ORDER';
-const DELETE_ORDER = 'DELETE_ORDER';
 
 /**
  * ACTION CREATORS
@@ -32,35 +28,8 @@ const addToCart = (product) => {
 
 const removeFromCart = (productId) => {
   return {
-    type: removeFromCart,
+    type: REMOVE_FROM_CART,
     productId
-  }
-}
-
-const getOrders = (orders) => {
-  return {
-    type: GET_ORDERS,
-    orders
-  }
-}
-
-const createOrder = (order) => {
-  return {
-    type: CREATE_ORDER,
-    order
-  }
-}
-
-const updateOrder = (order) => {
-  return {
-    type: UPDATE_ORDER,
-    order
-  }
-}
-
-const deleteOrder = (id) => {
-  return {
-    tyoe: DELETE_ORDER
   }
 }
 
@@ -80,65 +49,26 @@ export function createItem (item) {
   return function thunk (dispatch) {
     return axios.post('/api/cart', item)
     .then(res => res.data)
-    .then(item => dispatch(addToCart))
+    .then(newItem => dispatch(addToCart(newItem)))
   }
 }
 
-export function deleteItem (itemId) {
+export function deleteItem (productId) {
   return function thunk (dispatch) {
-    return axios.delete('/api/cart')
-    .then(item => dispatch(deleteFromCart(itemId)))
+    return axios.delete(`/api/cart/item/${productId}`)
+    .then(item => dispatch(removeFromCart(productId)))
   }
 }
-
-export function fetchOrders() {
-  return function thunk(dispatch) {
-    return axios.get('/api/order')
-    .then(res => res.data)
-    .then(orders => dispatch(getOrders(orders)))
-    .catch(err => console.error('Fetching orders failed.', err))
-  }
-}
-
-export function fetchOrder (id) {
-  return function thunk (dispatch) {
-    return axios.get(`/api/order/${id}`)
-    .then(res => res.data)
-    .then(order => dispatch(getOrder(order)))
-    .catch(err => console.error('Fetching order failed.', err))
-  }
-}
-
-export function addOrder (order) {
-  return function thunk (dispatch) {
-    return axios.post('/api/order', order)
-    .then(res => res.data)
-    .then(newOrder => dispatch(newOrder))
-    .catch(err => console.error('Adding order failed.', err))
-  }
-}
-
-export function editOrder (order) {
-  return function thunk (dispatch) {
-    return axios.put(`/api/order/${order.id}`)
-    .then(updatedOrder => dispatch(updatedOrder))
-    .catch(err => console.error('Updating order failed.', err))
-  }
-}
-
-export function removeOrder (id) {
-  return function thunk (dispatch) {
-    return axios.delete(`/api/order/${order.id}`)
-    .then(() => dispatch(deleteOrder(id)))
-    .catch(err => console.error('Deleting order unsuccessful.', err))
-    }
-  }
-
 
 export default function reducer (state = {}, action) {
   switch (action.type) {
     case GET_CART:
       return action.cart
+    case ADD_TO_CART:
+      return {...state, lineItems: [...state.lineItems, action.item]}
+    case REMOVE_FROM_CART:
+      const updatedItems = state.lineItems.filter(i => i.productId !== action.productId);
+      return {...state, lineItems: updatedItems}
     default:
       return state
   }
